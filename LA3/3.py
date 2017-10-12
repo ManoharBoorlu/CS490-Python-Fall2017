1cb9800dd0a0e3bed5d9cabf64a3c941b3cbf678
Manohar Boorlu <manohar.boorlu@gmail.com>
2017-10-11 23:25:00 -0500

    Update 3.py

--- LA3/3.py
+++ LA3/3.py
@@ -11,3 +11,12 @@ x_train,x_test,y_train,y_test=cv.train_test_split(x,y,test_size=0.3)   # Splitti
 model.fit(x_train, y_train)                             # fitting training data into model
 y_pred=model.predict(x_test)                            # predicting with the testing data
 print("Accuracy :", metrics.accuracy_score(y_test,y_pred))            # printing accuracy score
+
+diab = datasets.load_diabetes()                          # Loading the dataset
+model = svm.SVC(kernel='rbf')                         # Loading SVM Model
+x=diab.data
+y=diab.target
+x_train,x_test,y_train,y_test=cv.train_test_split(x,y,test_size=0.3)   # Splitting testing data as 30%
+model.fit(x_train, y_train)                             # fitting training data into model
+y_pred=model.predict(x_test)                            # predicting with the testing data
+print("RBF Accuracy :", metrics.accuracy_score(y_test,y_pred))            # printing accuracy score
